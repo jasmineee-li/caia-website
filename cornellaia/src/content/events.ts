2b33e52c72be61896e35e546b3e9a46bbfa5489a
Jinzhou Wu <jinzhouwu.public@gmail.com>
2026-02-13 13:47:48 +0000

    added openhouse to event list

--- cornellaia/src/content/events.ts
+++ cornellaia/src/content/events.ts
@@ -4,6 +4,7 @@ export const EVENTS_EMBED_URL =
   "https://luma.com/embed/calendar/cal-NrH7EP037bvvQtm/events?lt=light";
 
 export const PAST_EVENT_IDS = [
+  "jst12JWoIZq2MEY",
   "vfwzibwhJEsB2sx",
   "gZ1HLC8ZVDneIqZ",
   "IXGr5rIHSYR4urE",
